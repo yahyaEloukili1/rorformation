6c687a06e9f485b52e758e0ed6efc90aa983e856
yahya <yahya.eloukili11@gmail.com>
2021-07-07 14:52:26 +0100

    model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home', as: 'home'
-  get '/salut(/:name)', to: 'pages#salut', as: 'salut'
+  root(to: 'pages#home', as: 'home')
+   get('/salut(/:name)', to: 'pages#salut', as: 'salut')
+  get 'articles', to: 'posts#index', as: 'posts'
 end
